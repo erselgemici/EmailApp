24e1d8c650e4933091df08980213f13663bb06c0
Ersel <52753296+erselgemici@users.noreply.github.com>
2025-09-17 00:26:29 +0300

    Add files via upload

---
+++ Entities/Message.cs
@@ -0,0 +1,19 @@
+namespace EmailApp.Entities
+{
+    public class Message
+    {
+        public int? ReceiverId { get; set; }
+        public AppUser Receiver { get; set; }
+        public int SenderId { get; set; }
+        public AppUser Sender { get; set; }
+        public int MessageId { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public DateTime SendDate { get; set; }
+        public bool IsDraft { get; set; }   
+        public bool IsDeleted { get; set; } 
+        public bool IsRead { get; set; }   
+        public bool IsArchived { get; set; }   
+        public bool IsImportant { get; set; }   
+    }
+}
